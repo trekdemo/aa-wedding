252ba0424c55975323fa38852fb4d11fb9d53d13
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-03-09 14:31:28 +0100

    navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
   $('section:not([data-initial])').hide();
+
+  $('.header-container a').click(function(e){
+    var $link = $(this),
+        href  = $link.attr('href');
+
+    $('section').hide();
+    $(href).show();
+
+    $('.header-container nav li').removeClass('active');
+    $link.parent().addClass('active');
+
+    e.preventDefault();
+  });
 });
